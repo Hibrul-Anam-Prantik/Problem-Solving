30cda9d0d5f5cd9fb4ed9702c8e7b5328ad7d482
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-09-08 17:53:51 +0600

    Day02, Operators

---
+++ HackerRank_30DaysOfCode/Day02_Operators.java
@@ -0,0 +1,29 @@
+package HackerRank_30DaysOfCode;
+
+import java.util.Scanner;
+
+public class Day02_Operators 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        double meal_cost = sc.nextDouble();
+        int tip_percent = sc.nextInt();
+        int tax_percent = sc.nextInt();
+        sc.close();
+        TotalMealCost cost = new TotalMealCost();
+        cost.solve(meal_cost, tip_percent, tax_percent);
+    }
+}
+
+class TotalMealCost 
+{
+    public void solve(double meal_cost, int tip_percent, int tax_percent) 
+    {
+        double tip = meal_cost * (tip_percent*1.0)/100;
+        double tax = meal_cost * (tax_percent*1.0)/100;
+        double total = meal_cost + tip + tax;
+        int totalCost = (int) Math.round(total);   
+        System.out.println(totalCost);     
+    }
+}
