d462d2de1ac4895d2c66ecdd09d81540c0326ff7
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-09-09 09:46:59 +0600

    Day03 Conditional Ststements

---
+++ HackerRank_30DaysOfCode/Day03_IntroToConditionalStatements.java
@@ -0,0 +1,23 @@
+package HackerRank_30DaysOfCode;
+
+import java.util.Scanner;
+
+public class Day03_IntroToConditionalStatements {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        if(N % 2 != 0) {
+            System.out.println("Weird");
+        } else {
+            if(N >= 2 && N <=5) {
+                System.out.println("Not Weird");
+            } else if(N >= 6 && N <= 20) {
+                System.out.println("Weird");
+            } else if(N > 20) {
+                System.out.println("Not Weird");
+            }
+        }
+        sc.close();
+    }
+}
