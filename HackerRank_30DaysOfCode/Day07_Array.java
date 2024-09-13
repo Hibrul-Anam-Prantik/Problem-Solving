814d0ee37d45af3e78a2b5827a2a44ebebd18eb1
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-09-13 18:07:40 +0600

    Day07 array

---
+++ HackerRank_30DaysOfCode/Day07_Array.java
@@ -0,0 +1,10 @@
+public class Day07_Array {
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 1, 4, 5};
+
+        for (int i = arr.length - 1; i >= 0; i --) 
+        {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
