852b43934e01aa4f9f117226fb8c803d57ec15f9
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-09-30 14:48:46 +0600

    HackerRank > 30 Days of Code > Day13 > Abstract Classes

--- HackerRank_30DaysOfCode/Day13_AbstractClasses.java
+++ HackerRank_30DaysOfCode/Day13_AbstractClasses.java
@@ -1,6 +1,41 @@
 package HackerRank_30DaysOfCode;
 
-public class Day13_AbstractClasses
-{
+import java.util.Scanner;
 
+public class Day13_AbstractClasses {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String title = scanner.nextLine();
+        String author = scanner.nextLine();
+        int price = scanner.nextInt();
+        scanner.close();
+
+        Book book = new MyBook(title, author, price);
+        book.display();
+    }
+}
+
+abstract class Book {
+    String title;
+    String author;
+
+    Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    abstract void display();
+}
+
+class MyBook extends Book {
+    int price;
+
+    MyBook(String title, String author, int price) {
+        super(title, author);
+        this.price = price;
+    }
+
+    void display() {
+        System.out.println("Title: " + title + "\nAuthor: " + author + "\nPrice: " + price);
+    }
 }
