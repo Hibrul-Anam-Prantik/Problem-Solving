7582b437ad95198e984968ef053647231979b2f5
Hibrul-Anam-Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2024-10-07 16:39:29 +0600

    HackerRank > 30 Days of Code > Day16 > StringToInteger(Integer.parseInt(String)); TryCatchBlock

--- HackerRank_30DaysOfCode/Day16_StringToInteger.java
+++ HackerRank_30DaysOfCode/Day16_StringToInteger.java
@@ -1,4 +1,17 @@
 package HackerRank_30DaysOfCode;
 
-public class Day16_StringToInteger {
+import java.util.Scanner;
+
+public class Day16_StringToInteger
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String S = sc.nextLine();
+        sc.close();
+        try {
+            System.out.println(Integer.parseInt(S));
+        } catch (Exception e) {
+            System.out.println("Bad String");
+        }
+    }
 }
