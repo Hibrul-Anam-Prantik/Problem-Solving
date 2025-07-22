f6ac372d936bde1f8cacb92b2d26f18daa34daac
Hibrul Anam Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2025-07-22 23:44:33 +0600

    AddOrXOR

---
+++ Add_or_XOR.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Add_or_XOR {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt(); // number of test cases
+
+    }
+}
