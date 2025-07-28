3e208bb87d879413da65344a7d50917f309abb67
Hibrul Anam Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2025-07-28 11:32:51 +0600

    A_RecyclingCenter

--- Codeforces_Round_1039_Div2/A_RecyclingCenter.java
+++ Codeforces_Round_1039_Div2/A_RecyclingCenter.java
@@ -1,5 +1,7 @@
 package Codeforces_Round_1039_Div2;
 
+import java.util.Arrays;
+
 public class A_RecyclingCenter {
     public static void main(String[] args) throws Exception {
         java.io.BufferedReader br = new java.io.BufferedReader(new java.io.InputStreamReader(System.in));
@@ -9,17 +11,19 @@ public class A_RecyclingCenter {
             String[] line1 = br.readLine().split(" ");
             int n = Integer.parseInt(line1[0]);
             int c = Integer.parseInt(line1[1]);
-            int storedC = c;
             int[] w = new int[n];
             String[] weights = br.readLine().split(" ");
             for(int j = 0; j < n; j++) {
                 w[j] = Integer.parseInt(weights[j]);
             }
-            for(int j = 0; j < n; j++) {
-                long d = w[j] * exponentiation(2, j);
-                if(d > c) c--;
+            Arrays.sort(w);
+            int coins = 0;
+            for (int j = 0; j < n; j++) {
+                long weight = (long)w[j] * exponentiation(2, j); // after doubling j times
+                if (weight > c) coins++;
+                w[j] = 0;
             }
-            System.out.println(storedC - c);
+            System.out.println(coins);
         }
         br.close();
     }
@@ -37,3 +41,79 @@ public class A_RecyclingCenter {
         return result;
     }
 }
+// Accepted Solution:
+/*
+import java.util.*;
+
+public class One {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            long c= sc.nextLong();
+            long arr[] = new long[n];
+            for(int i=0; i<n; i++){
+                arr[i]=sc.nextLong();
+            }
+            ArrayList<Long>list = new ArrayList<>();
+
+            for(int i=0; i<n; i++){
+                if(arr[i]<=c){
+                    long val= (long)((Math.log(c)-Math.log(arr[i]))/Math.log(2));
+//                    System.out.println(val);
+                    list.add(val);
+                }
+            }
+
+            Collections.sort(list);
+
+            int cnt=0;
+            int time=0;
+            for(int i=0; i<list.size(); i++){
+                if(list.get(i)>=time){
+                    cnt++;
+                    time++;
+                }
+            }
+            System.out.println(n-cnt);
+        }
+    }
+}
+
+ */
+// Accepted Solution-2:
+/*
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+
+            int n=sc.nextInt();
+            int c=sc.nextInt();
+
+            int[] a= new int[n];
+            PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+            int big = 0;
+
+            for(int i=0;i<n;i++){
+                a[i]=sc.nextInt();
+                if(a[i]<=c) pq.offer(a[i]);
+                else    big++;
+            }
+
+            int power=0;
+            while(!pq.isEmpty()){
+                int  x=pq.poll();
+                if(x*Math.pow(2,power)>c)  big++;
+                else power++;
+            }
+            System.out.println(big);
+        }
+	}
+
+}
+ */
