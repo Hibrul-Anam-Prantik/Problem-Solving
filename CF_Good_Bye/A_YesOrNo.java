e1cfa9f0db230aa7e6c8a41cd2b70c58bd55b151
Hibrul Anam Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2025-12-27 23:37:29 +0600

    init

---
+++ CF_Good_Bye/A_YesOrNo.java
@@ -0,0 +1,10 @@
+package CF_Good_Bye;
+
+import java.io.*;
+
+public class A_YesOrNo {
+    public static void main(String[] args) throws IOException {
+        System.out.println();
+        
+    }
+}
